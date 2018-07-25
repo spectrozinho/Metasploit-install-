69b642846a0f04a3aedd09723ce13ede3b240e75
spectrozinho <41579139+spectrozinho@users.noreply.github.com>
2018-07-25 11:58:15 -0300

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,49 @@
+Detalhesapt update && apt upgrade
+
+apt install curl
+
+cd ..
+
+clear
+
+
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+
+chmod +x metasploit.sh
+
+
+
+echo "\n "
+#figura com oq ta escrito
+figlet   Install Metasploit
+
+echo "\033[1;31m              by: Spectro Hacking \033[0m"
+
+
+
+
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/me
+tasploit.sh
+
+chmod +x metasploit.sh
+
+echo "\033[1;31m       Instalando Metasploit...        \033[0m";sleep 2
+
+
+
+
+./metasploit.sh
+
+
+figlet istalado
+
+echo "\n\n\033[1;31m   Obrigado po usar meu script   \033[0m"
+
+echo "Youtube: https://www.youtube.com/channel/UCAmRS0i3PL2hlSNf8zpuKGg"
+echo \033[1;31m Passa la da um like ,Aproveita e se-escreva tambem\033[m0"
+
+
+
+figlet fim
+
+msfconsole
